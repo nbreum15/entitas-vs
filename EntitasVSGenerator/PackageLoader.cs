dff7d138451a87135fc917b826ef51bad7f003e1
Nicklas Breum <nbreum15@student.aau.dk>
2017-11-12 17:52:58 +0100

    Extension first loads when a solution is started

---
+++ EntitasVSGenerator/PackageLoader.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+
+namespace EntitasVSGenerator
+{
+    class PackageLoader : IVsSolutionEvents
+    {
+        private Action _solutionCloseCallback;
+        private Action _solutionOpenCallback;
+        private bool _solutionOpened;
+        private bool _solutionClosed;
+
+        public PackageLoader(Action solutionCloseCallback, Action solutionOpenCallback)
+        {
+            _solutionCloseCallback = solutionCloseCallback;
+            _solutionOpenCallback = solutionOpenCallback;
+        }
+
+        public int OnAfterCloseSolution(object pUnkReserved)
+        {
+            if (_solutionClosed)
+                return VSConstants.S_OK;
+            _solutionCloseCallback?.Invoke();
+            _solutionClosed = true;
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            if(_solutionOpened)
+                return VSConstants.S_OK;
+            _solutionOpenCallback?.Invoke();
+            _solutionOpened = true;
+            return VSConstants.S_OK;
+        }
+
+        #region Unused interface implementation
+        public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.S_OK;
+        }
+        #endregion
+    }
+}
