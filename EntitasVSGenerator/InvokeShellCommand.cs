51ae4d80194d0be11eb79252dae514c1c3ee9039
Nicklas Breum <nbreum15@student.aau.dk>
2017-11-12 11:20:18 +0100

    Fixed InvalidOperationException when BeginInvoke is called mutiple times

--- EntitasVSGenerator/InvokeShellCommand.cs
+++ EntitasVSGenerator/InvokeShellCommand.cs
@@ -1,36 +1,32 @@
 ﻿namespace EntitasVSGenerator
 {
     using System.Management.Automation;
-    using System.Management.Automation.Runspaces;
 
     class InvokeShellCommand
     {
         private readonly string _solutionDirectory;
         private readonly PowerShell _shellInstance;
-        private readonly Runspace _runspace;
+        private bool _alreadyRunning;
 
         public InvokeShellCommand(string solutionDirectory)
         {
             _solutionDirectory = solutionDirectory;
             _shellInstance = OpenShell();
-            _runspace = RunspaceFactory.CreateRunspace();
-            _runspace.Open();
+            _shellInstance.AddScript("entitas client 3333 gen -s");
         }
 
         private PowerShell OpenShell()
         {
             PowerShell powerShell = PowerShell.Create();
-            powerShell.AddScript($"set-location {_solutionDirectory}");
-            powerShell.Invoke();
             return powerShell;
         }
 
         public void Generate()
         {
-            _shellInstance.Runspace = _runspace;
-            //_shellInstance.AddScript("entitas client 3333 gen");
-            _shellInstance.AddScript("entitas gen");
-            _shellInstance.BeginInvoke();
+            if (_alreadyRunning)
+                return;
+            _alreadyRunning = true;
+            _shellInstance.BeginInvoke<object>(null, null, ar => _alreadyRunning = false, null);
         }
     }
 }
