0614964cbeb62310b975c5ebacbb608e05c9d31b
Nicklas Breum <nbreum15@student.aau.dk>
2017-11-14 18:33:22 +0100

    Changed title of extension window

--- EntitasVSGenerator/MainWindow.cs
+++ EntitasVSGenerator/MainWindow.cs
@@ -23,7 +23,7 @@
         /// </summary>
         public MainWindow() : base(null)
         {
-            this.Caption = "Window";
+            this.Caption = "Entitas Generator";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
