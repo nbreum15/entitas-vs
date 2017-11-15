64a1bc37983caa1dfbbd9d05e9a0786ce8e1ba6c
Nicklas Breum <nbreum15@student.aau.dk>
2017-11-15 23:32:08 +0100

    Tried some stuff with entitas dlls

---
+++ EntitasVSGenerator/Extensions/EntitasFactory.cs
@@ -0,0 +1,20 @@
+﻿using Entitas.CodeGeneration.CodeGenerator;
+using Entitas.Utils;
+
+namespace EntitasVSGenerator.Extensions
+{
+    static class EntitasFactory
+    {
+        public static Preferences GetPreferences(string solutionDirectory)
+        {
+            string propertiesPath = solutionDirectory + Preferences.DEFAULT_PROPERTIES_PATH;
+            string userPropertiesPath = solutionDirectory + Preferences.DEFAULT_USER_PROPERTIES_PATH;
+            return new Preferences(propertiesPath, userPropertiesPath);
+        }
+
+        public static CodeGenerator GetCodeGenerator(string solutionDirectory)
+        {
+            return CodeGeneratorUtil.CodeGeneratorFromPreferences(GetPreferences(solutionDirectory));
+        }
+    }
+}
