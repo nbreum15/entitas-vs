03b03da4570dacc5d882851c74b1b5b890b2683b
Nicklas Breum <nbreum15@student.aau.dk>
2017-11-15 23:30:21 +0100

    Created seperate class for common path handling

---
+++ EntitasVSGenerator/Extensions/PathUtil.cs
@@ -0,0 +1,23 @@
+﻿using EnvDTE;
+
+namespace EntitasVSGenerator.Extensions
+{
+    static class PathUtil
+    {
+        public const string SettingsName = "entitas-vs.cfg";
+
+        public static string GetSolutionDirectory(DTE dte)
+        {
+#if DEBUG
+            return @"C:\Users\nickl\Desktop\entitas-test";
+#else
+            return dte.Solution.FullName;
+#endif
+        }
+
+        public static string GetSettingsPath(DTE dte)
+        {
+            return $@"{GetSolutionDirectory(dte)}\{SettingsName}";
+        }
+    }
+}
