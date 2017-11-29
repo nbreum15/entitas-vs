79dc9ebe0f7c38c112cb5413b41f819421158003
Nicklas Breum <nbreum15@student.aau.dk>
2017-11-29 22:55:56 +0100

    Renamed package class

--- Entitas-vs.Main/EntitasVsPackage.cs
+++ Entitas-vs.Main/EntitasVsPackage.cs
@@ -4,10 +4,9 @@ using System.Runtime.InteropServices;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using System.IO;
 using EntitasVSGenerator.Extensions;
 using EntitasVSGenerator.Logic;
-using System.Reflection;
+using EnvDTE80;
 
 namespace EntitasVSGenerator
 {
@@ -15,21 +14,23 @@ namespace EntitasVSGenerator
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(MainWindow))]
-    [Guid(MainWindowPackage.PackageGuidString)]
+    [Guid(PackageGuidString)]
     [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
     [ProvideAutoLoad(UIContextGuids80.SolutionHasSingleProject)]
     [ProvideAutoLoad(UIContextGuids80.SolutionHasMultipleProjects)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    sealed class MainWindowPackage : Package
+    sealed class EntitasVsPackage : Package
     {
         public const string PackageGuidString = "9f4d054c-8bcc-4a90-ab81-3e1d00ff8a08";
 
-        public MainWindowPackage()
+        public EntitasVsPackage()
         {
         }
 
         protected override void Initialize()
         {
+            base.Initialize();
+            MainWindowCommand.Initialize(this, null);
             // PackageLoader loads package when a solution is loaded
             PackageLoader loader = new PackageLoader(OnSolutionLoad);
             var vsSolution = (IVsSolution)GetService(typeof(SVsSolution));
@@ -38,25 +39,16 @@ namespace EntitasVSGenerator
 
         private void OnSolutionLoad()
         {
-            base.Initialize();
             var dte = (DTE)GetService(typeof(DTE));
             var runningDocumentTable = new RunningDocumentTable(this);
             var vsFileChangeEx = (IVsFileChangeEx)GetService(typeof(SVsFileChangeEx));
 
-            try
-            {
-                string solutionDirectory = dte.Solution.GetDirectory();
-                var configFile = new ConfigFile(solutionDirectory);
-                var logicController = new LogicController(configFile, dte, runningDocumentTable, vsFileChangeEx);
-                logicController.Run();
+            string solutionDirectory = dte.Solution.GetDirectory();
+            var configFile = new ConfigFile(solutionDirectory);
+            var logicController = new LogicController(configFile, dte, runningDocumentTable, vsFileChangeEx);
+            logicController.Run();
 
-                MainWindowCommand.Initialize(this, logicController.Model);
-            }
-            catch (Exception e)
-            {
-                File.WriteAllLines($@"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\entitas-vs.log", new[] { e.ToString() });
-                throw;
-            }
+            MainWindowCommand.Instance.Model = logicController.Model;
         }
     }
 }
