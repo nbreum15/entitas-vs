ea1519007db9dd6f682bfd15bbe1057fd458ebd7
Nicklas Breum <nbreum15@student.aau.dk>
2017-12-22 00:06:17 +0100

    Deleted unused view models

--- Entitas-vs.Main/ViewLogic/ViewModels/TabViewModels/ProjectGroupTabViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EntitasVSGenerator.ViewLogic.ViewModels
-{
-    class ProjectGroupTabViewModel : EmptyTabViewModel
-    {
-        public ProjectGroupTabViewModel(SettingsViewModel settingsViewModel) : base("Projects", settingsViewModel)
-        {
-        }
-    }
-}
